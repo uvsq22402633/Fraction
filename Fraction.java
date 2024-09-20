a82559adf56da35c798777b44337664dee0329b7
Jonathan LONG <uvsq22402633@gmail.com>
2024-09-20 15:54:50 +0200

    test de la fonction toString()

---
+++ Fraction.java
@@ -0,0 +1,13 @@
+public class Fraction{
+    float x;
+    float y;
+
+    public Fraction(float x, float y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public String toString(){
+        return "Je suis une fraction.";
+    }
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world !");
+        Fraction fraction = new Fraction(0, 0);
+        System.out.println(fraction.toString());
     }
-    
 }
